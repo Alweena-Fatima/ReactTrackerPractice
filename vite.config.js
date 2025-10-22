635261ed64895ddd1720c7237477be7007c37dd1
Alweena-Fatima <alweenacse@gmail.com>
2025-10-23 02:56:40 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  
-})
+  base: '/ReactTrackerPractice/', // <-- Add this for GitHub Pages
+  plugins: [react(), tailwindcss()],
+});
