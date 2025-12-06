c8e0bf4682469677e398f4db479020a8cd05c052
Alweena-Fatima <alweenacse@gmail.com>
2025-12-06 17:19:42 +0530

    Fix vite config base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ReactTrackerPractice/', // <-- Add this for GitHub Pages
+ 
   plugins: [react(), tailwindcss()],
 });
